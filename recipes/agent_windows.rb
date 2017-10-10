8e773b0a586299139135b281e9098a858ee3bf85
Don O'Neill <don.oneill@apexlearning.com>
2017-10-10 14:23:23 -0700

    Post rebase cleanup

--- recipes/agent_windows.rb
+++ recipes/agent_windows.rb
@@ -16,24 +16,24 @@
 # TODO: make install location configurable
 
 windows_package 'newrelic-infra' do
-    action :install
-    source node['newrelic-infra']['windows_source'] if node['newrelic-infra']['windows_source']    
-    checksum node['newrelic-infra']['windows_checksum'] if node['newrelic-infra']['windows_checksum']
-    installer_type :msi
-  end
-  # Lay down newrelic-infra agent config
-  template 'C:\Program Files\New Relic\newrelic-infra\newrelic-infra.yml' do
-    source 'newrelic-infra.yml.erb'
-    variables(
-      'license_key' => node['newrelic-infra']['license_key'],
-      'display_name' => node['newrelic-infra']['display_name'],
-      'log_file' => node['newrelic-infra']['log_file'],
-      'verbose' => node['newrelic-infra']['verbose'],
-      'proxy' => node['newrelic-infra']['proxy']
-    )
-    notifies :restart, 'service[newrelic-infra]', :delayed
-  end
-  # Setup newrelic-infra service
-  service "newrelic-infra" do
-    action [:enable, :start]
-  end
+  action :install
+  source node['newrelic-infra']['windows_source'] if node['newrelic-infra']['windows_source']    
+  checksum node['newrelic-infra']['windows_checksum'] if node['newrelic-infra']['windows_checksum']
+  installer_type :msi
+end
+# Lay down newrelic-infra agent config
+template 'C:\Program Files\New Relic\newrelic-infra\newrelic-infra.yml' do
+  source 'newrelic-infra.yml.erb'
+  variables(
+    'license_key' => node['newrelic-infra']['license_key'],
+    'display_name' => node['newrelic-infra']['display_name'],
+    'log_file' => node['newrelic-infra']['log_file'],
+    'verbose' => node['newrelic-infra']['verbose'],
+    'proxy' => node['newrelic-infra']['proxy']
+  )
+  notifies :restart, 'service[newrelic-infra]', :delayed
+end
+# Setup newrelic-infra service
+service "newrelic-infra" do
+  action [:enable, :start]
+end

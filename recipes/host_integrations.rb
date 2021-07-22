e8b092908fca23e9a93865d66f0be83938b89ec9
zelaskov <marcin.zelasko@codetain.eu>
2021-07-22 19:17:51 +0200

    remove .to_s method

--- recipes/host_integrations.rb
+++ recipes/host_integrations.rb
@@ -14,7 +14,7 @@ if node['newrelic_infra']['features']['host_integrations'].any?
     package integration_name do
       action node['newrelic_infra']['packages'][integration_name]['action']
       retries node['newrelic_infra']['packages'][integration_name]['retries']
-      version node['newrelic_infra']['packages'][integration_name]['version'].to_s
+      version node['newrelic_infra']['packages'][integration_name]['version']
     end
   end
 

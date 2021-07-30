bb66e1363c851ec5259a63b4b4f338fe4676b7df
zelaskov <marcin.zelasko@codetain.eu>
2021-07-30 08:51:46 +0200

    change min ubuntu version

--- metadata.rb
+++ metadata.rb
@@ -8,12 +8,12 @@ long_description  IO.read(File.join(__dir__, 'README.md'))
 source_url        'https://github.com/newrelic/infrastructure-agent-chef'
 issues_url        'https://github.com/newrelic/infrastructure-agent-chef/issues'
 version           '0.11.0'
-chef_version      '>= 12.15'
+chef_version      '>= 15'
 
 # Platform support
 supports 'amazon', '>= 2013.0'
 supports 'debian', '>= 7.0'
-supports 'ubuntu', '>= 12.04'
+supports 'ubuntu', '>= 16.04'
 supports 'redhat', '>= 5.0'
 supports 'oracle', '>= 6.0'
 supports 'centos', '>= 5.0'

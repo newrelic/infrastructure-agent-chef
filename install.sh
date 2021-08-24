2b278e37f80907b6f6fe3456fac521896ae971ae
zelaskov <marcin.zelasko@codetain.eu>
2021-08-24 08:53:52 +0200

    infra: add basic github action for installing chef workstation

---
+++ install.sh
@@ -0,0 +1,8 @@
+VERSION=${1:-21.7.524}
+
+
+sudo apt-get install update -y
+sudo apt-get install git -y
+sudo apt-get install gpg -y
+wget https://packages.chef.io/files/stable/chef-workstation/${VERSION}/ubuntu/20.04/chef-workstation_${VERSION}-1_amd64.deb
+sudo dpkg -i chef-workstation_${VERSION}-1_amd64.deb

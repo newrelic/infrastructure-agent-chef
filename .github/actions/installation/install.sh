0f940dff9cb8574b78e419ddf64283503d3ec65e
zelaskov <marcin.zelasko@codetain.eu>
2021-09-07 18:12:46 +0200

    infra: install coreutils through script

--- .github/actions/installation/install.sh
+++ .github/actions/installation/install.sh
@@ -4,5 +4,6 @@ VERSION=${1:-21.7.524}
 sudo apt-get install update -y
 sudo apt-get install git -y
 sudo apt-get install gpg -y
+sudo apt-get install coreutils -y
 wget https://packages.chef.io/files/stable/chef-workstation/${VERSION}/ubuntu/20.04/chef-workstation_${VERSION}-1_amd64.deb
 sudo dpkg -i chef-workstation_${VERSION}-1_amd64.deb
